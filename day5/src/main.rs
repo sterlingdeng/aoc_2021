123d36001ca230d43d8e52ed6a88304d8ce7da2c
sterling <sterlingdeng@gmail.com>
2021-12-08 21:52:01 -0800

    day5_star2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -2,8 +2,8 @@ use std::fmt;
 use std::fs;
 
 struct PointPair {
-    start: [u32; 2],
-    end: [u32; 2],
+    start: [i32; 2],
+    end: [i32; 2],
 }
 
 impl PointPair {
@@ -14,14 +14,14 @@ impl PointPair {
         let raw_start: Vec<&str> = parts[0].trim().split(",").collect();
         let raw_end: Vec<&str> = parts[1].trim().split(",").collect();
 
-        let start_x: u32 = raw_start[0].parse().unwrap();
-        let start_y: u32 = raw_start[1].parse().unwrap();
+        let start_x: i32 = raw_start[0].parse().unwrap();
+        let start_y: i32 = raw_start[1].parse().unwrap();
 
-        let end_x: u32 = raw_end[0].parse().unwrap();
-        let end_y: u32 = raw_end[1].parse().unwrap();
+        let end_x: i32 = raw_end[0].parse().unwrap();
+        let end_y: i32 = raw_end[1].parse().unwrap();
 
-        let start: [u32; 2] = [start_x, start_y];
-        let end: [u32; 2] = [end_x, end_y];
+        let start: [i32; 2] = [start_x, start_y];
+        let end: [i32; 2] = [end_x, end_y];
 
         Self { start, end }
     }
@@ -29,9 +29,9 @@ impl PointPair {
 
 struct PointPairs {
     points: Vec<PointPair>,
-    size: [u32; 2], // [x, y]
-    matrix: Vec<Vec<u32>>,
-    result: u32,
+    size: [i32; 2], // [x, y]
+    matrix: Vec<Vec<i32>>,
+    result: i32,
 }
 
 impl fmt::Display for PointPairs {
@@ -67,14 +67,9 @@ impl PointPairs {
         self.matrix = vec![vec![0; x + 1]; y + 1];
     }
 
-    fn process(&mut self) -> u32 {
+    fn process(&mut self) -> i32 {
         for point in self.points.iter() {
-            // println!("Point: {:?} -> {:?}", point.start, point.end);
-            if !is_horizontal_or_vertical(point.start, point.end) {
-                continue
-            }
             let points_between = get_points_between(point.start, point.end);
-            // println!("Points between: {:?}", points_between);
             for (_, point_between) in points_between.iter().enumerate() {
                 let x = point_between[0] as usize;
                 let y = point_between[1] as usize;
@@ -88,11 +83,14 @@ impl PointPairs {
     }
 }
 
-fn is_horizontal_or_vertical(a: [u32; 2], b: [u32; 2]) -> bool {
+fn is_horizontal_or_vertical(a: [i32; 2], b: [i32; 2]) -> bool {
     a[0] == b[0] || a[1] == b[1]
 }
 
-fn get_points_between(a: [u32; 2], b: [u32; 2]) -> Vec<[u32; 2]> {
+fn get_points_between(a: [i32; 2], b: [i32; 2]) -> Vec<[i32; 2]> {
+    if a[0] > b[0] {
+        return get_points_between(b, a);
+    }
     let [x1, y1] = a;
     let [x2, y2] = b;
     let reorder = |x, y| {
@@ -102,18 +100,36 @@ fn get_points_between(a: [u32; 2], b: [u32; 2]) -> Vec<[u32; 2]> {
         return [x, y];
     };
 
-    let mut result: Vec<[u32; 2]> = Vec::new();
+    let mut result: Vec<[i32; 2]> = Vec::new();
 
     if x1 == x2 {
+        // vertical line
         let [low, high] = reorder(y1, y2);
         for i in low..high + 1 {
             result.push([x1, i]);
         }
         return result;
-    } else {
+    } else if y1 == y2 {
+        // horizontal line
         let [low, high] = reorder(x1, x2);
         for i in low..high + 1 {
             result.push([i, y1]);
+        }
+        return result;
+    } else {
+        let slope = (y2 - y1) / (x2 - x1);
+        let [low_x, high_x] = reorder(x1, x2);
+        if slope > 0 {
+            // diag \ (+ve slope)
+            for c in low_x..=high_x {
+                result.push([c, y1 + c - low_x]);
+            }
+        } else {
+            // diag / (-ve slope)
+            let k = x1 + y1;
+            for x in low_x..=high_x {
+                result.push([x, k - x]);
+            }
         }
         return result;
     }
@@ -134,6 +150,5 @@ fn main() {
     }
     point_pairs.init_matrix();
     let result = point_pairs.process();
-    // println!("matrix: \n{}", point_pairs);
     println!("result: {}", result);
 }
